d91a47891014bb1c37bffa94c28473cb16ad73c4
artemwwwq <artem201190@mail.ru>
2022-11-09 22:31:56 +0500

    Update mindmachine.js

--- mindmachine.js
+++ mindmachine.js
@@ -3,12 +3,11 @@ let scores = [
   64, 66, 55, 52, 61, 46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44,
 ];
 
-var costs = [.25, .27, .25, .25, .25, .25, 
-  .33, .31, .25, .29, .27, .22, 
-  .31, .25, .25, .33, .21, .25, 
-  .25, .25, .28, .25, .24, .22,
-  .20, .25, .30, .25, .24, .25, 
-  .25, .25, .27, .25, .26, .29];
+var costs = [
+  0.25, 0.27, 0.25, 0.25, 0.25, 0.25, 0.33, 0.31, 0.25, 0.29, 0.27, 0.22, 0.31,
+  0.25, 0.25, 0.33, 0.21, 0.25, 0.25, 0.25, 0.28, 0.25, 0.24, 0.22, 0.2, 0.25,
+  0.3, 0.25, 0.24, 0.25, 0.25, 0.25, 0.27, 0.25, 0.26, 0.29,
+];
 
 function printAndGetHighScore(scores) {
   let output;
@@ -41,23 +40,21 @@ console.log("Highest bubble score: " + highScore);
 let bestSolutions = getBestResults(scores, highScore);
 console.log("Solutions with the highest score: " + bestSolutions);
 
-
-function getMostCosteffectiveSolution (scores, costs, highScore) {
+function getMostCosteffectiveSolution(scores, costs, highScore) {
   let cost = 100;
   let index;
-  for (i = 0; i<scores.length; i++) {
+  for (i = 0; i < scores.length; i++) {
     if (scores[i] == highScore) {
-
-    
-   if (scores[i] < cost ) {
-    index = i;
-    cost = scores[i];
-  }
-  }
+      if (scores[i] < cost) {
+        index = i;
+        cost = scores[i];
+      }
+    }
   }
   return index;
 }
 
-
- let mostCostEffective = getMostCosteffectiveSolution (scores, costs, highScore);
-console.log("Bubble Solution #" + mostCostEffective + " is the most cost effective");
+let mostCostEffective = getMostCosteffectiveSolution(scores, costs, highScore);
+console.log(
+  "Bubble Solution #" + mostCostEffective + " is the most cost effective"
+);
