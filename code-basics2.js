462191803e5986cd6732de6b6dc63e908b0e4ee9
artemwwwq <artem201190@mail.ru>
2022-11-10 21:41:22 +0500

    Create code-basics2.js

---
+++ code-basics2.js
@@ -0,0 +1,10 @@
+// JavaScript: Обход строк
+// BEGIN (write your solution here)
+const printReversedWordBySymbol = (slovo) => {
+    let i = 1;
+    while (i <= slovo.length) {
+      console.log(slovo[slovo.length - i]);
+      i = i + 1;
+    }
+  }
+  // END
